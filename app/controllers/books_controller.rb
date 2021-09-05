d05e2f8b0af3fde3ba2deec76e0c5b01efe143cc
ichi-a00 <prenehru@gmail.com>
2021-09-05 11:30:28 +0000

    [FIX]3-9 フラッシュメッセとバリデーションOK

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,9 +6,19 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to books_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all #ルーティングを通らないので@books=Book.allが飛ばない、どうするか？？？
+      #binding.pry
+      render :index
+    end
+
+    #book = Book.new(book_params)
+    #book.save
+    #redirect_to books_path(book.id)
   end
 
   def show
@@ -24,9 +34,16 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
+    #book = Book.find(params[:id])
+    #book.update(book_params)
+    #redirect_to book_path(book)
   end
 
   def destroy
